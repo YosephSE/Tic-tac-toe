96ed01abb88769daf873adf56d00505ef950500d
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-06-07 14:49:51 +0300

    Dom variable added

---
+++ static/main.js
@@ -0,0 +1,12 @@
+const cells = document.querySelectorAll(".cell");
+const winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+let board = ["", "", "", "", "", "", "", "", ""];
