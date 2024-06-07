e8e9b4f718948bbc56c1a7669792149fe4350b50
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-06-07 15:34:40 +0300

    basic functionality achieved

--- static/main.js
+++ static/main.js
@@ -1,4 +1,6 @@
 const cells = document.querySelectorAll(".cell");
+const statusText = document.getElementById("currentPlayer");
+const restartBtn = document.getElementById("restart");
 const winConditions = [
     [0, 1, 2],
     [3, 4, 5],
@@ -10,3 +12,70 @@ const winConditions = [
     [2, 4, 6]
 ];
 let board = ["", "", "", "", "", "", "", "", ""];
+
+let currentPlayer = "X";
+let running = false;
+
+initializeGame();
+
+function initializeGame(){
+    cells.forEach(cell => cell.addEventListener("click", cellClicked));
+    restartBtn.addEventListener("click", restartGame);
+    statusText.textContent = `${currentPlayer}'s turn`;
+    running = true;
+}
+function cellClicked(){
+    const index = this.getAttribute("index");
+
+    if(board[index] != "" || !running){
+        return;
+    }
+
+    updateCell(this, index);
+    checkWinner();
+}
+function updateCell(cell, index){
+    board[index] = currentPlayer;
+    cell.textContent = currentPlayer;
+}
+function changePlayer(){
+    currentPlayer = (currentPlayer == "X") ? "O" : "X";
+    statusText.textContent = `${currentPlayer}'s turn`;
+}
+function checkWinner(){
+    let roundWon = false;
+
+    for(let i = 0; i < winConditions.length; i++){
+        const condition = winConditions[i];
+        const cellA = board[condition[0]];
+        const cellB = board[condition[1]];
+        const cellC = board[condition[2]];
+
+        if(cellA == "" || cellB == "" || cellC == ""){
+            continue;
+        }
+        if(cellA == cellB && cellB == cellC){
+            roundWon = true;
+            break;
+        }
+    }
+
+    if(roundWon){
+        statusText.textContent = `${currentPlayer} wins!`;
+        running = false;
+    }
+    else if(!board.includes("")){
+        statusText.textContent = `Draw!`;
+        running = false;
+    }
+    else{
+        changePlayer();
+    }
+}
+function restartGame(){
+    currentPlayer = "X";
+    board = ["", "", "", "", "", "", "", "", ""];
+    statusText.textContent = `${currentPlayer}'s turn`;
+    cells.forEach(cell => cell.textContent = "");
+    running = true;
+}
