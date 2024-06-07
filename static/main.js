50d907ff93ce28edcd12d3ba9e8529852bd507d0
IamHannibal <hannibalyo@gmail.com>
2024-06-07 22:52:36 +0300

    update cell

--- static/main.js
+++ static/main.js
@@ -41,4 +41,9 @@ function cellClicked(){
     checkWinner();
 }
 
+function updateCell(cell, index){
+    board[index] = currentPlayer;
+    cell.textContent = currentPlayer;
+}
+
 initializeGame();
